f46f4d95fb40e4cb2402043c23a3c530181b261b
rachel613g <rachelgutmann613@gmail.com>
2020-10-25 15:58:11 -0400

    removed extra call to mSnackBar.show()

--- app/src/main/java/com/example/echoapp/MainActivity.java
+++ app/src/main/java/com/example/echoapp/MainActivity.java
@@ -48,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void handleFABClick() {
         mSnackBarOutput.setText(mEditTextInput.getText().toString());
-        mSnackBarOutput.show();
     }
 
     private void setUpToolbar() {
